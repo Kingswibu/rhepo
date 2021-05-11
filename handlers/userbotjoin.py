038bddb29b11774c947173db2833f900fee6c10f
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-05-12 04:19:28 +0700

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -13,7 +13,7 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>Add me as admin of yor group first</b>",
+            "<b>Jadikan Saya Sebagai Admin Di grup Anda.</b>",
         )
         return
 
@@ -24,21 +24,21 @@ async def addchannel(client, message):
 
     try:
         await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id,"I joined here as you requested")
+        await USER.send_message(message.chat.id,"Saya Bergabung di Grup anda")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>@asistan_random already in your chat</b>",
+            "<b>@asistan_random Suda Siap Dalam Chat</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @asistan_random to your Group and try again</b>",
+            "\n\nOr Undang secara manual  @asistan_random Ke grup Anda sekai lagi</b>",
         )
         return
     await message.reply_text(
-            "<b>@asistan_random userbot joined your chat</b>",
+            "<b>@asistan_random userbot Telah Bergabung Di Grup Anda</b>",
         )
     
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
@@ -47,7 +47,7 @@ async def rem(USER, message):
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>User couldn't leave your group! May be floodwaits."
-            "\n\nOr manually kick me from to your Group</b>",
+            f"<b>Saya Keluar Dari Grup anda, Mungkin Karna sudah Terlalu banyak Pemakaian."
+            "\n\nOr Keluarkan Saya Secara Manual</b>",
         )
         return
