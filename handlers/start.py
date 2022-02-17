82706e4e9629d08128362a4fb13f71c08f8573e0
Kingswibu <99636581+Kingswibu@users.noreply.github.com>
2022-02-17 10:37:25 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -11,35 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def start_(client: Client, message: Message):
     await message.reply_sticker("CAACAgQAAx0CVij2LgABEzNHYK73j8kBtxw0_SoHsMzUQ5gEu6MAAn4MAAJLae4QXlcTPAtFOZsfBA")
     await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name}!
-\nSaya Adalah Bot yang akan memainkan Musik Di VCG Group Anda
-Saya di Buat Oleh [RAMA](https://t.me/maafgausahsokap) ❤
-\nJangan Lupa Follow IG creator saya @ramadh20 😍
-\nHit /help list of available commands.
- </b>""",
-      
-       
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "OWNER", url="https://t.me/MaafGausahSokap",
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/teman_random"
-                    ),
-                    InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/ramubotinfo"
-                    ),
-                    InlineKeyboardButton(
-                        "MY ASISTANT", url="https://t.me/random_asisstan"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "➕ Add To Your Group ➕", url="https://t.me/Random_musikkbot?startgroup=true"
+        
                     ) 
                 ]
             ]
@@ -47,13 +19,13 @@ Saya di Buat Oleh [RAMA](https://t.me/maafgausahsokap) ❤
     )
 
 @Client.on_message(
-    filters.command("start")
+    filters.command("_")
     & filters.group
     & ~ filters.edited
 )
 async def start(client: Client, message: Message):
     await message.reply_text(
-        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+      
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -80,31 +52,7 @@ async def start(client: Client, message: Message):
 )
 async def help(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name}!
-\n/play <song name> - play song you requested
-/dplay <song name> - play song you requested via deezer
-/splay <song name> - play song you requested via jio saavn
-/playlist - Show now playing list
-/current - Show now playing
-/song <song name> - download songs you want quickly
-/search <query> - search videos on youtube with details
-/deezer <song name> - download songs you want quickly via deezer
-/saavn <song name> - download songs you want quickly via saavn
-/video <song name> - download videos you want quickly
-\n*Admins only*
-/player - open music player settings panel
-/pause - pause song play
-/resume - resume song play
-/skip - play next song
-/end - stop music play
-/userbotjoin - invite assistant to your chat
-/admincache - Refresh admin list
- </b>""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/candu_musikk"
+        
                     )
                 ]
             ]
